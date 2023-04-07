9de911d7270de33fe42550e014ce67ea06785653
baronhoot <baronhoot@gmail.com>
2023-04-07 18:38:50 -0500

    update run scripts

--- rtl_433_mqtt_autodiscovery/run.sh
+++ rtl_433_mqtt_autodiscovery/run.sh
@@ -52,4 +52,7 @@ else
 fi
 
 echo "Starting rtl_433_mqtt_hass.py..."
-python3 -u /rtl_433_mqtt_hass.py -H $MQTT_HOST -p $MQTT_PORT -R "$RTL_TOPIC" -D "$DISCOVERY_PREFIX" -i $DISCOVERY_INTERVAL $OTHER_ARGS
+
+
+echo "Running: python3 -u /rtl_433_mqtt_hass.py -r -d -u ${MQTT_USERNAME} -P ${MQTT_PASSWORD} -H ${MQTT_HOST} -p ${MQTT_PORT} -D ${DISCOVERY_PREFIX} -R ${RTL_TOPIC} -i ${DISCOVERY_INTERVAL} ${OTHER_ARGS}"
+python3 -u /rtl_433_mqtt_hass.py -r -d -u ${MQTT_USERNAME} -P ${MQTT_PASSWORD} -H ${MQTT_HOST} -p ${MQTT_PORT} -D ${DISCOVERY_PREFIX} -R ${RTL_TOPIC} -i $DISCOVERY_INTERVAL $OTHER_ARGS
